4e06e357bc0cf046488656f8c065af4bbd794a77
James Porter <james@amimetic.co.uk>
2015-12-28 23:34:46 +0000

    some basic example things done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
-import RaisedButton from 'material-ui/lib/raised-button';
-import { MyThemeMixin } from './customTheme'
 
 export const App = React.createClass({
-  mixins: [MyThemeMixin],
   render() {
     return (
-      <div>
-        <h1>Empty App!</h1>
-        <RaisedButton label="Yo" primary={true}/>
+      <div className="container">
+        <div className="header">
+          <h1>New Year Countdown</h1>
+        </div>
+
+        <div className="main">
+          <h1>Empty App!</h1>
+        </div>
+
+        <div className="footer">
+          <h3>For Web Dashboard</h3>
+          <a href="https://play.google.com/store/apps/details?id=uk.amimetic.webdashboard">Use with Web Dashboard (Android App)</a>
+
+        </div>
+
       </div>
     );
   }
