653ec2437a2e4143ff5aaabcfd7ba3f6948cd983
James Porter <james@amimetic.co.uk>
2015-12-29 00:21:30 +0000

    very basic dashboard done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import React, { Component } from 'react';
 
 export const App = React.createClass({
-  render() {
-    return (
-      <div className="container">
-        <div className="header">
-          <h1>New Year Countdown</h1>
-        </div>
+    componentDidMount(){
+      setInterval(() => {
+        let now = new Date();
+        if(now.getFullYear() < this.state.countdownTo.getFullYear()) {
+            this.setState({now});
+        } else {
+            let countdownTo = new Date("01/01/" + (now.getFullYear()+1));
+            this.setState({now, countdownTo});
+        }
+      }, 1000);
+    },
+    getInitialState(){
+        let now = new Date();
+        let year = now.getFullYear();
+        let countdownTo = new Date("01/01/" + (year+1));
+        return { countdownTo, now };
+    },
+    render() {
+        const distance = this.state.countdownTo - this.state.now;
 
-        <div className="main">
-          <h1>Empty App!</h1>
-        </div>
+        const days = Math.floor(distance / (3600000*24));
+        const hours = Math.floor((distance % (3600000*24)) / 3600000);
+        let minutes = Math.floor((distance % 3600000) / 60000);
+        let seconds = Math.floor((distance % 60000) / 1000);
 
-        <div className="footer">
-          <h3>For Web Dashboard</h3>
-          <a href="https://play.google.com/store/apps/details?id=uk.amimetic.webdashboard">Use with Web Dashboard (Android App)</a>
+        if(minutes < 10) minutes = "0" + minutes;
+        if(seconds < 10) seconds = "0" + seconds;
 
-        </div>
+        return (
+          <div className="container">
+            <div className="header">
+              <h1>New Year Countdown</h1>
+            </div>
 
-      </div>
-    );
-  }
+            <div className="main">
+              <h1 className="countdown">{days} days {hours}:{minutes}:{seconds}</h1>
+            </div>
+
+            <div className="footer">
+              <h3>For Web Dashboard</h3>
+              <a href="https://play.google.com/store/apps/details?id=uk.amimetic.webdashboard">Use with Web Dashboard (Android App)</a>
+
+            </div>
+
+          </div>
+        );
+    }
 });
